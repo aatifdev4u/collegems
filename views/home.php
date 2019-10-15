72cb58742403df444477ffdc95d7ccf45ea577f2
Mohammad Aatif <aatif@ipost.com>
2019-10-15 13:25:27 +0530

    add the JS and CSS file

---
+++ views/home.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>college management system</title>
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css');?>"/>
+	<script src="<?php echo base_url('assets/js/jquery-3.1.0.js');?>"></script>
+	<script src="<?php echo base_url('assets/js/bootstrap.js'); ?>"></script>
+</head>
+<body>
+
+<div id="container">
+	<h2>Welcome to college management system </h2>
+</div>
+
+</body>
+</html>
